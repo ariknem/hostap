6be643caca49b0ee53973d83a7f9cfb0a6eca8cd
Eyal Shapira <eyal@wizery.com>
2012-05-16 15:41:04 +0300

    Disconnect if we can't roam following beacon loss
    The driver reports the 1st beacon loss event but doesn't
    disconnect until further consecutive beacon loss events are
    received. That's being done in order to give the supplicant time
    to roam seamlessly (i.e. without reporting DISCONNECTED to upper
    layers). However, in case we can't find any roaming candidates
    it's better to disconnect as soon as possible so any supplicant
    client will be able to switch quickly to another communication
    channel (e.g. mobile 3G). This change initiates up to two immediate
    specific SSID scans on all channels following a beacon loss in order
    to locate a roaming candidate. Two scans are performed in case
    a roaming candidate was preivously blacklisted. That way, it'll be
    explored if there's no other option.
    
    Signed-off-by: Eyal Shapira <eyal@wizery.com>

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -1374,6 +1374,21 @@ static int wpas_select_network_from_last_scan(struct wpa_supplicant *wpa_s,
 			 */
 			int timeout_sec = wpa_s->scan_interval;
 			int timeout_usec = 0;
+
+			if (wpa_s->roaming && (wpa_s->num_roaming_scans > 1)) {
+				wpa_dbg(wpa_s, MSG_DEBUG, "No roaming candidate"
+					" found. Attempt another scan");
+				wpa_s->num_roaming_scans--;
+				wpa_supplicant_req_new_scan(wpa_s, 0, 0);
+				return 0;
+			} else if (wpa_s->roaming) {
+				wpa_dbg(wpa_s, MSG_DEBUG,
+					"No roaming candidate found "
+					"after multiple scans. Disconnect");
+				wpa_supplicant_deauthenticate(wpa_s,
+					      WLAN_REASON_DEAUTH_LEAVING);
+				/* Fall through so periodic scan won't stop */
+			}
 #ifdef CONFIG_P2P
 			if (wpas_p2p_scan_no_go_seen(wpa_s) == 1)
 				return 0;
@@ -2688,6 +2703,7 @@ static void wpas_event_deauth(struct wpa_supplicant *wpa_s,
 
 	wpas_event_disconnect(wpa_s, addr, reason_code,
 			      locally_generated, ie, ie_len, 1);
+	wpa_s->roaming = 0;
 }
 
 
@@ -3194,6 +3210,7 @@ void wpa_supplicant_event(void *ctx, enum wpa_event_type event,
 	case EVENT_START_ROAMING:
 		if (!is_zero_ether_addr(wpa_s->bssid)) {
 			wpa_s->roaming = 1;
+			wpa_s->num_roaming_scans = 2;
 			wpa_supplicant_req_scan(wpa_s, 0, 0);
 		}
 		break;

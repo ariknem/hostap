3965399db8b9e4d383442d7b2fd68c1e3249fd88
Eliad Peller <eliad@wizery.com>
2014-11-24 18:19:55 +0200

    smart_config: sync ti_vendor_cmd.h with kernel
    There was some mismatch
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>

--- src/drivers/ti_vendor_cmd.h
+++ src/drivers/ti_vendor_cmd.h
@@ -6,17 +6,6 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA
- *
  */
 
 #ifndef __WLCORE_VENDOR_H__
@@ -38,7 +27,6 @@ enum wlcore_vendor_commands {
 };
 
 enum wlcore_vendor_attributes {
-	WLCORE_VENDOR_ATTR_SMART_CONFIG_EVENT,
 	WLCORE_VENDOR_ATTR_FREQ,
 	WLCORE_VENDOR_ATTR_PSK,
 	WLCORE_VENDOR_ATTR_SSID,

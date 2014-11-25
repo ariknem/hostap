354366e87041b0b7fa76cb45b2f5d31a993aad30
Eliad Peller <eliad@wizery.com>
2014-11-25 16:35:40 +0200

    ACS: don't choose JP-only channel
    Avoid using channels 38 and 42 which are valid
    only in JP.
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>

--- src/ap/hw_features.c
+++ src/ap/hw_features.c
@@ -1094,6 +1094,10 @@ static int valid_ap_channel(struct hostapd_iface *iface, int chan)
 	if (chan == 14)
 		return 0;
 
+	/* don't allow JP-only channels */
+	if (chan == 38 || chan == 42)
+		return 0;
+
 	/* don't allow channels on the the ACS blacklist */
 	if (iface->conf->acs_blacklist) {
 		list = iface->conf->acs_blacklist;
